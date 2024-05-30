6bcfaf6af6527ab9c2ae449209b7157b56018c36
Youfili <Youfili@github.com>
2024-05-30 18:25:01 +0200

    Sistemato il ChoosePlayerPanel, inserimento del tasto "Back Menu" per tornare alla scelta del tipo di partita nel caso si volesse cambiare la modalita "Nuova o Continua partita"

---
+++ src/Swing/menuFrame/battle/ScoreOfBattles.java
@@ -0,0 +1,14 @@
+package Swing.menuFrame.battle;
+
+import javax.swing.*;
+
+/*
+Creo questo pannello per avere in tempo reale il punteggio delle partite giocate, quando uno dei due giocatori arriva a 3 vittorie
+terminano le Battaglie, si salvano i progressi e poi si decide se svolgere altri combattimenti (da capo) o chiudere il gioco
+ */
+public class ScoreOfBattles extends JPanel {
+
+
+
+
+}
